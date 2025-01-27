efe2e8a54181a5ad39675be3aec3b52683de15cb
DeanCode <ds@xxxmail.eu>
2025-01-27 16:16:39 +0100

    Updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="bg-gray-800 text-white p-4 text-center">
       <p className="text-sm">
-        © 2023 DarkPatterns'R'Us. All Rights Reserved*
+        © 2025 DarkPatterns'R'Us. All Rights Reserved*
       </p>
       <p className="text-xs mt-2">
         *By using this site, you agree to sell your soul and firstborn child.
