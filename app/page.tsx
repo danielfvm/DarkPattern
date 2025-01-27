9d8981cc39c52fdef778a1b15f90ee9b910804b7
DeanCode <ds@xxxmail.eu>
2025-01-27 15:47:57 +0100

    Fixed error again

--- app/page.tsx
+++ app/page.tsx
@@ -105,7 +105,7 @@ export default function Home() {
           </p>
         </div>
 
-        {(<Joyride
+        {domLoaded && (<Joyride
           steps={steps}
           hideBackButton
           hideCloseButton
